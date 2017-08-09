bf94496255d48258a174087cba0bf976ae7de1f3
Enrico <enricosaccon96@gmail.com>
2017-08-09 18:04:49 +0200

    Today is a good day

---
+++ SDReader/SDReader.ino
@@ -0,0 +1,83 @@
+/*
+  SD card read/write
+
+ This example shows how to read and write data to and from an SD card file
+ The circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+
+ created   Nov 2010
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain. 
+ */
+ 
+#include <SPI.h>
+#include <SD.h>
+#include <TMRpcm.h>
+
+TMRpcm tmrpcm;
+
+void setup(){
+  tmrpcm.speakerPin = 9;
+  Serial.begin(9600);
+  if (!SD.begin(7)) {
+    Serial.println("SD fail");
+    return;
+  }
+  else {
+    Serial.println("Yeah");
+  }
+  tmrpcm.setVolume(4);
+  tmrpcm.play("Allarm.wav");
+}
+
+void loop(){  }
+
+
+//
+//File myFile;
+//
+//void setup() {
+//  // Open serial communications and wait for port to open:
+//  Serial.begin(9600);
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+//  Serial.print("Initializing SD card...");
+//
+//  if (!SD.begin(4)) {
+//    Serial.println("initialization failed!");
+//    return;
+//  }
+//  Serial.println("initialization done.");
+//
+//  // open the file. note that only one file can be open at a time,
+//  // so you have to close this one before opening another.
+//  myFile = SD.open("prova.txt", FILE_READ);
+//  Serial.println(myFile);
+//  if (myFile) {
+//    Serial.println("prova.txt:");
+//
+//    // read from the file until there's nothing else in it:
+//    while (myFile.available()) {
+//      Serial.write(myFile.read());
+//    }
+//    // close the file:
+//    myFile.close();
+//  } else {
+//    // if the file didn't open, print an error:
+//    Serial.println("error opening prova.txt");
+//  }
+//}
+//
+//void loop() {
+//  // nothing happens after setup
+//}
+
+
